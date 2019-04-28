02118fcf1de468b69105982d4b705cefcbe71a38
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-168-122-221-157.bu.edu>
2019-04-28 17:34:53 -0400

    Model and image save in server

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+import seaborn as sns
+
+from PIL import Image
+
+torch.manual_seed(42)
+np.random.seed(42)
+
 
 app = Flask(__name__)
 
@@ -8,12 +20,21 @@ app = Flask(__name__)
 def home_dir():
     return "Welcome to Home Dir"
 
-@app.route("/photo_ML", methods = ['GET'])
+
+@app.route("/photo_ML", methods = ['POST'])
 def photo_classify():
     print("Endpoint Reached")
     print(request.files)
     img_data = {request.files}
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['./data/images'], filename))
     return "Image Received?", 200
+
+@app.route("/location")
+def fetch_location():
+    print("Fetching Hospital Info from API...")
+    
 
 
 if __name__ == "__main__":
