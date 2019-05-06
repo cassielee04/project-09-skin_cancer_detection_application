f3fd1c80dde32bd4c92e2c6dad9e4628b2627d8e
ByoungSul lee <byoungsullee@ByoungSului-MacBook-Pro.local>
2019-05-06 00:21:09 -0400

    Google map API added with Boston Coords

--- Upload_server/Server.js
+++ Upload_server/Server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const router = express.Router();
 const port = 3000
 
 app.get('/Upload', (request, response) => {
   response.send('Front Page')
+  // https://www.w3schools.com/html/html5_geolocation.asp
 })
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname+'/Gmap.html'));
+  })
 
 app.get('/Receive', (request, response) => {
     response.send('Where it receiveds and shows results')
