68c84bead13dc163262a640b73c5aade01874aae
seunghee <seunghee@bu.edu>
2019-05-06 01:42:59 -0400

    routings for web

--- Upload_server/Server.js
+++ Upload_server/Server.js
@@ -3,24 +3,46 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 var path = require('path');
+var formidable = require('formidable');
+
+var httpmodule = require('http');
+var url1 = "http://1bfa1f4e.ngrok.io/photo_ML";
+var fs = require('fs');
+const fileSystemModule = require("tns-core-modules/file-system")
 
 const port = 3000
 
 
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname + '/view'));
 app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug');
 
 
 app.get('/',(req,res)=>{  
-  console.log("Welcome to CareSkin",req.cookies)
+  console.log("Welcome to CareSkin")
   res.render(__dirname + '/view/index.pug');
 
 });
 
-app.get('/Upload', (request, response) => {
-  response.send('Front Page')
+app.get('/Upload', (req, res) => {
+  res.render(__dirname + '/view/upload.pug');
+})
+
+
+app.post('/detection', (req, res) => {
+  console.log("we are detecting")
+  // var formData = {
+
+  //   file: fs.createReadStream('testImage_2.jpg')
+  // };
+  // request.post({url:"http://1bfa1f4e.ngrok.io/photo_ML", formData: formData}, function optionalCallback(err, httpResponse, body) {
+  //   if (err) {
+  //     return console.error('upload failed:', err);
+  //   }
+  //   console.log('Upload successful!  Server responded with:', body);
+
+  res.render(__dirname + '/view/detection.pug');
 })
 
 app.get('/Receive', (request, response) => {
